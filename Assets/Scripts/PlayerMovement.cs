bfaeafd809bcbc7345b421f3238e719c6266bf6f
marco <marcomaraglino5@gmail.com>
2024-10-25 16:42:00 +0200

    Added Water Physics

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,22 +9,29 @@ namespace Assets.Scripts
         public CharacterController controller;
 
         public float speed = 12f;
+        public float swimSpeed = 6f; // Velocità ridotta per il nuoto
         public float gravity = -9.81f * 2;
+        public float waterGravity = -2f; // Gravità ridotta in acqua
         public float jumpHeight = 3f;
 
         public Transform groundCheck;
         public float groundDistance = 0.4f;
         public LayerMask groundMask;
+        public LayerMask waterMask; // Maschera per rilevare l'acqua
+
+        public Transform waterSurface; // Punto che indica la superficie dell'acqua
+        public float maxSwimHeight = 0.5f; // Altezza massima sopra la superficie dell'acqua
 
         Vector3 velocity;
-
         bool isGrounded;
+        bool isInWater;
 
         // Update is called once per frame
         void Update()
         {
-            //checking if we hit the ground to reset our falling velocity, otherwise we will fall faster the next time
+            // Controllo se il giocatore è a terra o in acqua
             isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            isInWater = Physics.CheckSphere(groundCheck.position, groundDistance, waterMask);
 
             if (isGrounded && velocity.y < 0)
             {
@@ -34,19 +41,45 @@ namespace Assets.Scripts
             float x = Input.GetAxis("Horizontal");
             float z = Input.GetAxis("Vertical");
 
-            //right is the red Axis, foward is the blue axis
             Vector3 move = transform.right * x + transform.forward * z;
 
-            controller.Move(move * speed * Time.deltaTime);
-
-            //check if the player is on the ground so he can jump
-            if (Input.GetButtonDown("Jump") && isGrounded)
+            if (isInWater)
             {
-                //the equation for jumping
-                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            }
+                // Movimenti in acqua
+                controller.Move(move * swimSpeed * Time.deltaTime);
 
-            velocity.y += gravity * Time.deltaTime;
+                // Limite della risalita in acqua
+                float maxHeight = waterSurface.position.y + maxSwimHeight;
+
+                if (Input.GetButton("Jump") && transform.position.y < maxHeight)
+                {
+                    // Consente di salire solo fino al limite della superficie
+                    velocity.y = swimSpeed * 0.5f; // Risalita controllata
+                }
+                else if (transform.position.y >= maxHeight)
+                {
+                    // Se il giocatore è troppo in alto, lo manteniamo alla superficie
+                    velocity.y = Mathf.Min(velocity.y, 0);
+                }
+                else
+                {
+                    // Applicazione della gravità in acqua
+                    velocity.y += waterGravity * Time.deltaTime;
+                }
+            }
+            else
+            {
+                // Movimenti a terra o in aria
+                controller.Move(move * speed * Time.deltaTime);
+
+                // Salto se il giocatore è a terra
+                if (Input.GetButtonDown("Jump") && isGrounded)
+                {
+                    velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                }
+
+                velocity.y += gravity * Time.deltaTime;
+            }
 
             controller.Move(velocity * Time.deltaTime);
         }
