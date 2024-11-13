67a709a344eed2d660e2478ee6cd2184b89d50b8
marco <marcomaraglino5@gmail.com>
2024-11-13 19:40:22 +0100

    rabbit + snowy

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -1,49 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+ 
 public class SelectionManager : MonoBehaviour
 {
-    public GameObject InteractionInfo;
-    private TextMeshProUGUI interaction_text;
-    public float interactionDistance = 3f; // Distance to show interaction text
-
+ 
+    public GameObject interaction_Info_UI;
+     TextMeshProUGUI interaction_text;
+ 
     private void Start()
     {
-        interaction_text = InteractionInfo.GetComponent<TextMeshProUGUI>();
-        InteractionInfo.SetActive(false); // Hide the interaction info at start
+        interaction_text = interaction_Info_UI.GetComponent<TextMeshProUGUI>();
     }
-
-void Update()
-{
-    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-    RaycastHit hit;
-
-    if (Physics.Raycast(ray, out hit))
+ 
+    void Update()
     {
-        var selectionTransform = hit.transform;
-        Debug.Log("Hit object: " + selectionTransform.name); // Log the name of the hit object
-
-        InteractableObject interactable = selectionTransform.GetComponent<InteractableObject>();
-        Debug.Log("Interactable component: " + interactable); // Log the interactable component
-
-        // Check if the player is close enough to the interactable object
-        if (interactable && Vector3.Distance(transform.position, selectionTransform.position) <= interactionDistance)
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 10))
         {
-            InteractionInfo.SetActive(true);
-            interaction_text.text = interactable.GetItemName();
-            Debug.Log("Displaying info for: " + interactable.GetItemName()); // Log when displaying info
-        }
-        else
-        {
-            InteractionInfo.SetActive(false);
+            var selectionTransform = hit.transform;
+ 
+            if (selectionTransform.GetComponent<InteractableObject>())
+            {
+                Debug.Log(selectionTransform.GetComponent<InteractableObject>().GetItemName());
+                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
+                interaction_Info_UI.SetActive(true);
+            }
+            else 
+            { 
+                interaction_Info_UI.SetActive(false);
+            }
+ 
         }
     }
-    else
-    {
-        InteractionInfo.SetActive(false); // Hide UI if nothing is hit
-    }
-}
 }
