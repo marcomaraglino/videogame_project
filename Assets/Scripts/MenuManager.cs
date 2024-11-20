13557fd2cd5db988a2b541a2bf129b466f1296c3
Luketh127 <pierfabionotarnicola@gmail.com>
2024-11-20 22:11:14 +0100

    progress nei menu
    molto importante ricordare di implementare successivamente il crafting system come condizione nell'if dell'ingame menu una volta creato

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance { get; set; }
+
+    public GameObject menuCanvas;
+    public GameObject UICanvas;
+
+    public GameObject saveMenu;
+    public GameObject settingsMenu;
+    public GameObject menu;
+
+    public bool isMenuOpen;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && !isMenuOpen)
+        {
+            UICanvas.SetActive(false);
+            menuCanvas.SetActive(true);
+
+            isMenuOpen = true;
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            SelectionManager.Instance.DisableSelection();
+            SelectionManager.Instance.GetComponent<SelectionManager>().enabled = false;
+
+        }else if (Input.GetKeyDown(KeyCode.Z) && isMenuOpen)
+        {
+
+            saveMenu.SetActive(false);
+            settingsMenu.SetActive(false);
+            menu.SetActive(true);
+
+            UICanvas.SetActive(true);
+            menuCanvas.SetActive(false);
+
+            isMenuOpen = false;
+
+            if (InventorySystem.Instance.isOpen == false)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+            
+            SelectionManager.Instance.EnableSelection();
+            SelectionManager.Instance.GetComponent<SelectionManager>().enabled = true;
+        }
+    }
+}
