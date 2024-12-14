26ff61695022faade40e27fea656ea437f5263e6
marco <marcomaraglino5@gmail.com>
2024-12-14 20:36:00 +0100

    sound manager created & fixed dd

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+
+
+{
+
+    public static SoundManager Instance { get; set; }
+    // Start is called before the first frame update
+    public AudioSource dropItemSound;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
